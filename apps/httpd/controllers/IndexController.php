65c584400a2c61275e35345485f68b36ea909db5
胡志超 <zhichao.hu@xianghuanji.com>
2019-01-02 10:55:37 +0800

    敏感词过滤

---
+++ apps/httpd/controllers/IndexController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace apps\httpd\controllers;
+
+use mix\http\Controller;
+use apps\httpd\libraries\doubleArrayTrie;
+
+/*
+|--------------------------------------------------------------------------
+| 敏感词过滤
+|--------------------------------------------------------------------------
+|
+| snippet
+|
+|
+*/
+class IndexController extends Controller
+{
+    // 默认动作
+    public function actionIndex()
+    {
+        return 'Hello, World!';
+    }
+
+    public function actionTest()
+    {
+    	return json_encode(['code' => 0, 'message' => 'OK', 'data' => app()->getPublicPath()]);
+    	//print_r(app()->getPublicPath());
+    	//return 'test';
+    }
+    
+    /**
+     * 添加  可以从MySQL || TXT文档 读敏感词库
+     *
+     * @var mixed
+     */
+    public function actionAdd()
+	{
+		$content = app()->request->get('content') ?? '草';
+
+        //敏感词数组 可以录读第五部分的词库 然后生成敏感词文件
+        $words = [$content];
+        //创建一个空的trie tree
+        $tire = \trie_filter_new();
+        //向trie tree中添加敏感词
+        foreach ($words as $k => $v) {
+            trie_filter_store($tire, $v);
+        }
+        //生成敏感词文件
+        trie_filter_save($tire, app()->getPublicPath() . 'dict.tree');
+
+        return json_encode(['code' => 0, 'message' => 'OK']);
+	}
+
+    /**
+     * 获取替换之后的敏感词
+     *
+     * @var mixed
+     */
+	public function actionGet() 
+	{
+		$content = app()->request->get('content') ?? '傻逼草拟吗 哈哈哈';
+		//\trie_filter_new();
+
+		//准备要过滤的文本
+		//$content = '傻逼草拟吗 哈哈哈';
+
+		$result = [];
+
+	    if (!empty($content)) {
+
+	        // 字典树文件路径，默认当时目录下
+	        $tree_file = app()->getPublicPath() . 'dict.tree';
+
+	        // 清除文件状态缓存
+	        clearstatcache();
+
+	        // 获取请求时，字典树文件的修改时间
+	        $new_mtime = filemtime($tree_file);
+
+	        // 获取最新trie-tree对象
+	        $resTrie = doubleArrayTrie::getResTrie($tree_file, $new_mtime);
+
+	        // 执行过滤
+	        $arrRet = trie_filter_search_all($resTrie, $content);
+
+	        // 提取过滤出的敏感词
+	        $result = doubleArrayTrie::getFilterWords($content, $arrRet);
+
+
+	        $badword = array_combine($result,array_fill(0,count($result),'***'));  
+
+			$str = strtr($content, $badword);  
+
+	    }
+
+		//释放trie资源
+		//trie_filter_free($tire);
+
+		return ['code' => 0, 'message' => 'OK', 'data' => $str];
+	}
+
+}
---
+++ apps/httpd/libraries/doubleArrayTrie.php
@@ -0,0 +1,66 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| 敏感词过滤  基于Double-Array Trie 
+|--------------------------------------------------------------------------
+|
+| snippet
+|
+|
+*/
+
+namespace apps\httpd\libraries;
+
+class doubleArrayTrie {
+
+	// trie-tree对象
+    private static $_resTrie = null;
+    // 字典树的更新时间
+    private static $_mtime = null;
+
+
+ 	/**
+     * 提供trie-tree对象
+     *
+     * @param $tree_file 字典树文件路径
+     * @param $new_mtime 当前调用时字典树的更新时间
+     * @return null
+     */
+    static public function getResTrie($tree_file, $new_mtime) {
+
+        if (is_null(self::$_mtime)) {
+            self::$_mtime = $new_mtime;
+        }
+
+        if (($new_mtime != self::$_mtime) || is_null(self::$_resTrie)) {
+            self::$_resTrie = trie_filter_load($tree_file);
+            self::$_mtime = $new_mtime;
+
+            // 输出字典文件重载时间
+            echo date('Y-m-d H:i:s') . "\tdictionary reload success!\n";
+        }
+
+        return self::$_resTrie;
+    }
+
+    /**
+     * 从原字符串中提取过滤出的敏感词
+     *
+     * @param $str 原字符串
+     * @param $res 1-3 表示 从位置1开始，3个字符长度
+     * @return array
+     */
+    static public function getFilterWords($str, $res)
+    {
+        $result = array();
+        foreach ($res as $k => $v) {
+            $word = substr($str, $v[0], $v[1]);
+
+            if (!in_array($word, $result)) {
+                $result[] = $word;
+            }
+        }
+
+        return $result;
+    }
+}
